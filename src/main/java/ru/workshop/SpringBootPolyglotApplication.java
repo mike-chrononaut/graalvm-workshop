ddc6238f1a0589b6e0117dccbab1f1b161115be4
m.yakovlev <m.yakovlev@iac.spb.ru>
2022-05-05 15:56:20 +0300

    Added initial dependencies

---
+++ src/main/java/ru/workshop/SpringBootPolyglotApplication.java
@@ -0,0 +1,11 @@
+package ru.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPolyglotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPolyglotApplication.class, args);
+    }
+}
