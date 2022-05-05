3446407621ba1bc6b3ddb6c61e3c85c55089c0de
m.yakovlev <m.yakovlev@iac.spb.ru>
2022-05-05 13:39:59 +0300

    Added final program with all native-image configs

--- src/main/java/ru/workshop/App.java
+++ src/main/java/ru/workshop/App.java
@@ -1,8 +1,24 @@
 package ru.workshop;
 
+import java.io.IOException;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        String root = "D:\\projects\\workshops\\graalvm-workshop";
+
+        if (args.length > 0) {
+            root = args[0];
+        }
+
+        final FileCount count = ListDir.list(root);
+        final String size = ListDir.humanReadableByteCountBin(count.getSize());
+
+        System.out.println("Counting directory: " + root);
+        System.out.println("Total: "
+                + count.getCount()
+                + " files, total size = "
+                + size);
     }
 }
